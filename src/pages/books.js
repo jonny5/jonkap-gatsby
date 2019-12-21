af7888f3a2bb36862e73aa8927cbb8ad91d11d7e
jonny5 <jonwiththewind@gmail.com>
2019-12-20 19:23:00 -0800

    Adding profile pic and books

---
+++ src/pages/books.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import RobotImage from "../images/j5.gif"
+import * as moment from 'moment'
+import {Helmet} from "react-helmet";
+
+class BooksPage extends React.Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
